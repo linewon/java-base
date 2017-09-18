7b8b580480dabfbaf00d15855e4393fe75cfb0d1
zcwang <zchwang@chinaums.com>
2017-09-18 19:51:07 +0800

    bufferReader test

---
+++ linesDemos/src/test/java/line/io/file/BufferedReaderDemo.java
@@ -0,0 +1,24 @@
+package line.io.file;
+
+public class BufferedReaderDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+	@SuppressWarnings("finally")
+	public static String readFile(String path) {
+		String result = "FILE DOESN'T EXIST!";
+		
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			return result;
+		}
+	}
+}
